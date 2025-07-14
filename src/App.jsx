ac12a8ddb4420a7af3917f251e1807430f8459a4
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-07-14 18:28:17 +0530

    new component add

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import './App.css'
 import Calculator from "./components/Calculator.jsx";
+import SumVersion1 from "./components/Sum_version_1.jsx";
+import SumVersion2 from "./components/Sum_version_2.jsx";
+import SumVersion3 from "./components/Sum_version_3.jsx";
+import SumVersion4 from "./components/Sum_version_4.jsx";
+import SumVersion5 from "./components/Sum_version_5.jsx";
 
 function App() {
 
