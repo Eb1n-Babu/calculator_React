c9946ccaeb9303f27f38ca15b03ac411fb2cac76
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-07-14 18:26:41 +0530

    structural change for the component Calculator

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 
 function Calculator() {
-    const [num, setNum] = React.useState({num1: 0, num2: 0,sum: 0,sub: 0,div: 0,mul: 0});
+    const [num, setNum] = React.useState({num1: 0, num2: 0,result: 0});
 
     const onchange = (e) => {
         setNum({...num,[e.target.name]: e.target.value});
@@ -13,11 +13,11 @@ function Calculator() {
         <div className="calculator">
             <label>num1 :<input name="num1" type="text" placeholder="enter a number" onChange={onchange} /></label><br/>
             <label>num2 :<input name="num2" type="text" placeholder="enter a number" onChange={onchange} /></label><br/>
-            <button onClick={()=>setNum({...num,sum:parseFloat(num.num1)+parseFloat(num.num2)})}>+</button>
-            <button onClick={()=>setNum({...num,sum:parseFloat(num.num1)-parseFloat(num.num2)})}>-</button>
-            <button onClick={()=>setNum({...num,sum:parseFloat(num.num1)*parseFloat(num.num2)})}>*</button>
-            <button onClick={()=>setNum({...num,sum:parseFloat(num.num1)/parseFloat(num.num2)})}>/</button>
-            <h1>{num.sum}</h1>
+            <button onClick={()=>setNum({...num,result:parseFloat(num.num1)+parseFloat(num.num2)})}>+</button>
+            <button onClick={()=>setNum({...num,result:parseFloat(num.num1)-parseFloat(num.num2)})}>-</button>
+            <button onClick={()=>setNum({...num,result:parseFloat(num.num1)*parseFloat(num.num2)})}>*</button>
+            <button onClick={()=>setNum({...num,result:parseFloat(num.num1)/parseFloat(num.num2)})}>/</button>
+            <h1>{num.result}</h1>
         </div>
     );
 }
