c150933ff7beef371545841bbd75baea09538a7b
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-07-14 14:07:01 +0530

    Initial commit with project config and source files

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Calculator() {
+    const [number, setNumber] = React.useState({number_one:'',
+        number_two:'',sum:'',sub:'',mul:'',div:''});
+    const sumOf = () =>{
+        const num1 = number.number_one
+        const num2 = number.number_two
+        const result = (num1 + num2)
+        setNumber({...number,sum: result})
+    }
+    return (
+        <div>
+            <label>enter a number <input type="text" placeholder="enter first number "
+            value={number.number_one}   onChange={(e)=>{e.target.value}}/></label>
+            <label>enter a number <input type="text" placeholder="enter second number "
+            value={number.number_two}      onChange={(e)=>{e.target.value}}/></label>
+            <button onClick={sumOf}>+</button>
+            <h1>{number.sum}</h1>
+        </div>
+    );
+}
+
+export default Calculator;
